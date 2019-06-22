65409bf3b1d6ae0c8b3d5f223c98e59a5a6ea451
Ridwan Al-Mansur <ridwan.al.mansur@hotmail.com>
2019-06-22 17:10:52 +0100

    Add subtitle to infoview

--- Team5/Team5/HomeList/ARModel.swift
+++ Team5/Team5/HomeList/ARModel.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct ARModel {
     let title: String
+    let subtitle: String
     let image: UIImage
     let description: String
 }
@@ -23,10 +24,12 @@ struct HomeListDataSource {
     let sections = [
         Sections(sectionTitle: "", models: [
             ARModel(title: "Quran",
+                    subtitle: "",
                     image: UIImage(named: "quran")!,
                     description: "The Qur'an, sometimes spelled Koran, (Arabic: القرآن) is the holy book of Islam. The Qur'an is considered by Muslims to be 'The Word of Allah (God)'. This book is different from other religious texts in that it is believed to be written directly by God, through the prophet Muhammad. Some Muslims call it the Final Testament."),
 
             ARModel(title: "Badshahi Mosque",
+                    subtitle: "Pakistan",
                     image: UIImage(named: "badshahi-mosque")!,
                     description: "Commissioned by Emperor Aurangzeb in 1671, with construction of the mosque lasting for two years until 1673. The mosque is an important example of Mughal architecture, with an exterior that is decorated with carved red sandstone with marble inlay. It remains the largest and most recent of the grand imperial mosques of the Mughal-era, and is the second-largest mosque in Pakistan. After the fall of the Mughal Empire, the mosque was used as a garrison by the Sikh Empire and the British Empire, and is now one of Pakistan's most iconic sights.")
         ])
