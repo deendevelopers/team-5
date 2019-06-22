02e6aeecc111b7d9d007d702ec8b0cd2507e1d24
Rasheed <rasheedwihaib@gmail.com>
2019-06-22 12:10:01 +0100

    base project

---
+++ Team5/Team5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Team5
+//
+//  Created by Rasheed Wihaib on 22/06/2019.
+//  Copyright © 2019 Rasheed Wihaib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
